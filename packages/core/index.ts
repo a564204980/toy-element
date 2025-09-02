d944c0ec9d78b9ef684def940ef78f4dd696c662
梦归何处 <1140928814@qq.com>
2025-09-02 22:01:49 +0800

    🎈 perf: 构建时增加watch

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,8 +1,11 @@
+import printLogo from "./printLogo";
 import components from "./components";
 import "@toy-element-clone/theme/index.css";
 import { makeInstaller } from "@toy-element-clone/utils";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+
+printLogo()
 
 library.add(fas); // 添加所有图标
 const installer = makeInstaller(components);
--- packages/play/vite.config.ts
+++ packages/play/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // 定义全局变量
+  define: {
+    PROD: JSON.stringify(false),
+    DEV: JSON.stringify(false),
+    TEST: JSON.stringify(true),
+  },
+});
