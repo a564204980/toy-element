ae9c4a5f3cf82a07b8448745f377af8551f3cb8f
梦归何处 <1140928814@qq.com>
2025-09-11 12:02:53 +0800

    📃 docs: tooltip文档完善

--- packages/docs/demo/tooltip/Slot.vue
+++ packages/docs/demo/tooltip/Slot.vue
@@ -1,9 +1,6 @@
 <template>
   <er-tooltip placement="top">
-    <template #content>
-      <div>
-        <p>这是一个提示框</p>
-      </div>
-    </template>
+    <template #content>这是一个提示框</template>
+    <er-button>Top center</er-button>
   </er-tooltip>
 </template>
