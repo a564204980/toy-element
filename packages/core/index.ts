2faec03dce9b30d2440b0f4cd23d14d5a2e8f4f6
梦归何处 <1140928814@qq.com>
2025-10-29 17:24:26 +0800

    🎈 perf: release-it

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -7,5 +7,5 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 const installer = makeInstaller(components);
 
-export * from "./components"; // 导出所有组件，可以按需引入
+export * from "@toy-element/components"; // 导出所有组件，可以按需引入
 export default installer; // 导出安装器，用于Vue应用中安装所有组件
