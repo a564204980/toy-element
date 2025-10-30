b51df431316ae127d234502aa8bac610483051fc
梦归何处 <1140928814@qq.com>
2025-10-30 14:36:05 +0800

    🎈 perf: 一些优化

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -7,5 +7,7 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 const installer = makeInstaller(components);
 
-export * from "@toy-element/components"; // 导出所有组件，可以按需引入
+// export * from "@toy-element/components"; // 导出所有组件，可以按需引入
+// 这里引入组件不能用上面那种写法，因为最终通过build构建的产物dist的types里面core包是需要指向相对路径的components的
+export * from "../components"; 
 export default installer; // 导出安装器，用于Vue应用中安装所有组件
